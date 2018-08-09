a974e0df7a8f1392293a2231ed9881d2ff4b858a
rodolfotiago <rodolfotiago@gmail.com>
2018-08-09 13:57:05 -0400

    fixing output

--- oficial.sh
+++ oficial.sh
@@ -19,12 +19,10 @@ pdf_itatiba="http://www.itatiba.sp.gov.br/templates/midia/Imprensa_Oficial/${ano
 echo "buscando informacoes do site:"
 echo "${pdf_itatiba}"
 
-#if ls ./*.pdf* 1> /dev/null 2>&1; then
-#if ls | grep -E "[0-9]{2}.[0-9]{2}.[0-9]{4}.pdf" 1> /dev/null 2>&1; then
-if ls "${pdf_name}" 1> /dev/null 2>&1; then
+if ls "${pdf_name}"; then
 	echo "pdf de hoje encontrado..."
 	echo "executando primeiro teste..."
-	find . -iname '*.pdf' -exec pdfgrep -i "${pTest}" {} + 1> /dev/null 2>&1
+	pdfgrep -i "${pTest}" ${pdf_name}
 	if [[ "$?" -eq "0" ]]; then
 		echo "primeiro teste executado com sucesso!!!"
 		else
@@ -32,14 +30,21 @@ if ls "${pdf_name}" 1> /dev/null 2>&1; then
 			exit -1	
 	fi
 	echo "procurando padrao: ${pattern}..."
-	find . -iname '*.pdf' -exec pdfgrep -i "${pattern}" {} +
-	if [[ "$?" -ne "0" ]]; then
-		echo "padrao nao foi encontrado!!!"
+	pdfgrep -i "${pattern}" ${pdf_name}
+	if [[ "$?" -eq "0" ]]; then
+		echo "padrao encontrado..."
+		else
+			echo "padrao nao foi encontrado!!!"
 	fi
 	else 
 		echo "baixando o pdf de hoje..."
+		wget ${pdf_itatiba} 1> /dev/null 2>&1
+		if [[ "$?" -ne "0" ]]; then
+			echo "hoje nao houve registro no diario oficial de itatiba"
+			exit -1
+		fi
 		echo "executando primeiro teste..."
-		find . -iname '*.pdf' -exec pdfgrep -i "${pTest}" {} +
+		pdfgrep -i "${pattern}" ${pdf_name}
 		if [[ "$?" -eq "0" ]]; then
 			echo "primeiro teste executado com sucesso!!!"
 			else
@@ -47,14 +52,13 @@ if ls "${pdf_name}" 1> /dev/null 2>&1; then
 				exit -1
 		fi
 		echo "procurando padrao: ${pattern}..."
-		wget ${pdf_itatiba} 1> /dev/null 2>&1
-		if [[ "$?" -ne "0" ]]; then
-			echo "hoje nao houve registro no diario oficial de itatiba"
-			exit -1
-		fi
-		find . -iname '*.pdf' -exec pdfgrep -i "${pTest}" {} + 1> /dev/null 2>&1
-		if [[ "$?" -ne "0" ]]; then
-			echo "padrao nao foi encontrado!!!"
+		pdfgrep -i "${pattern}" ${pdf_name}
+		if [[ "$?" -eq "0" ]]; then
+			echo "padrao encontrado..."
+			else
+				echo "padrao nao foi encontrado!!!"
 		fi
 fi
-
+echo "script end"
+sleep 5
+exit 0
