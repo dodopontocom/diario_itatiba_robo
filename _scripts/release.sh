2968b5c8392836a6a48239e33c146ce07f81f3da
Rodolfo Neto <rodolfotiago@gmail.com>
2018-10-19 12:46:33 -0300

    Update and rename docker_push.sh to release.sh

---
+++ _scripts/release.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+IMAGE=bot-itatiba
+
+docker run --rm "${DOCKER_USERNAME}"/bump patch
+version=$(cat VERSION)
+echo "version: ${version}
+
+./build.sh
+
+git add -A
+git commit -m "version ${version}"
+git tag "${version}" -m "version ${version}"
+git push
+git push --tags
+
+echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+docker push "${DOCKER_USERNAME}/${IMAGE}:latest"
+docker push "${DOCKER_USERNAME}/${IMAGE}:${version}"
