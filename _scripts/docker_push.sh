77c0b8ee08da78665da43acbcf62fadb2883647f
Rodolfo Neto <rodolfotiago@gmail.com>
2018-10-19 11:56:01 -0300

    Update docker_push.sh

--- _scripts/docker_push.sh
+++ _scripts/docker_push.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 repo=bit-pipeline
-version=1.0.2
+version=1.0.1
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 docker push "${DOCKER_USERNAME}/${repo}:${version}"
