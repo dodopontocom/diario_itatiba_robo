e72a80fc94bcdf125db529359997152c0b10f32e
Rodolfo Neto <rodolfotiago@gmail.com>
2018-10-24 17:28:25 -0300

    Update bot.sh
    [skip ci]

--- _scripts/bot.sh
+++ _scripts/bot.sh
@@ -12,15 +12,7 @@ do
     do
     # bloco de instruções
     (
-	if [[ -s ${sourceFile} ]]; then
-		ShellBot.sendMessage --chat_id ${message_chat_id[$id]} --text "$(cat ${sourceFile})"
-		if [[ -s ${pdfFile} ]]; then
-			ShellBot.sendDocument --chat_id $usuario_id --document @${pdfFile}
-		fi
-		else
-			ShellBot.sendMessage --chat_id ${message_chat_id[$id]} --text "😔 ops, nao foi encontrado registro no diário oficial ainda hoje"
-			ShellBot.sendMessage --chat_id ${message_chat_id[$id]} --text "aguarde nas terças/quintas ou sábados, eu vou te atualizar com certeza!"	
-		fi
+	ShellBot.sendMessage --chat_id ${message_chat_id[$id]} --text "${chat_id}"
     ) &  # Criando thread
     done
 done
