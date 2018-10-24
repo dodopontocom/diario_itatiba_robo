805d6b37788fc2af78170923f05dd11d4a6680ce
Rodolfo Neto <rodolfotiago@gmail.com>
2018-10-24 17:25:57 -0300

    Update bot.sh
    [skip ci]

--- _scripts/bot.sh
+++ _scripts/bot.sh
@@ -1,1 +1,26 @@
-#!/bin/bash
+#/bin/bash
+
+#chamar o bot
+source ${BASEDIR}/../ShellBot.sh
+
+ShellBot.init --token $(cat ${BASEDIR}/token.txt) --monitor --return value
+
+while :
+do
+    # Lista as atualizações.
+    for id in $(ShellBot.ListUpdates)
+    do
+    # bloco de instruções
+    (
+	if [[ -s ${sourceFile} ]]; then
+		ShellBot.sendMessage --chat_id ${message_chat_id[$id]} --text "$(cat ${sourceFile})"
+		if [[ -s ${pdfFile} ]]; then
+			ShellBot.sendDocument --chat_id $usuario_id --document @${pdfFile}
+		fi
+		else
+			ShellBot.sendMessage --chat_id ${message_chat_id[$id]} --text "😔 ops, nao foi encontrado registro no diário oficial ainda hoje"
+			ShellBot.sendMessage --chat_id ${message_chat_id[$id]} --text "aguarde nas terças/quintas ou sábados, eu vou te atualizar com certeza!"	
+		fi
+    ) &  # Criando thread
+    done
+done
