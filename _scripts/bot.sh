e57ed5d05dff2cc47dce0637047f479b666fe42b
Rodolfo Neto <rodolfotiago@gmail.com>
2018-10-24 18:29:15 -0300

    Update bot.sh
    [skip ci]

--- _scripts/bot.sh
+++ _scripts/bot.sh
@@ -1,6 +1,10 @@
 #/bin/bash
 #
 BASEDIR=$(dirname "$0")
+apiUrl="https://api.travis-ci.org/repo/dodopontocom%2Fdiario_itatiba_robo/requests"
+branch="develop"
+token=$(cat ${BASEDIR}/travis_token.txt)
+
 #chamar o bot
 source ./ShellBot.sh
 
@@ -18,6 +22,21 @@ do
     (
     	ShellBot.sendMessage --chat_id ${message_chat_id[$id]} --text "${message_chat_id[$id]}"
 	ShellBot.sendMessage --chat_id ${message_chat_id[$id]} --text "\"${message_text}\""
+	
+	#send travis API request to trigger a build
+	body='{
+	"request": {
+	"message": "${message_chat_id[$id]}:\"${message_text}\"",
+	"branch":"${branch}",
+	}}'
+
+	curl -X POST -H "Content-Type: application/json" \
+	-H "Travis-API-Version: 3" \
+	-H "Accept: application/json" \
+	-H "Authorization: token ${token}" \
+	-d "${body}" \
+	${apiUrl}
+	
     ) &  # Criando thread
     done
 done
