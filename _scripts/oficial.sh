66040c485a2b4b9ab7ed86219d0d6aa4f69d820e
Rodolfo Neto <rodolfotiago@gmail.com>
2019-02-12 09:32:03 -0200

    Update oficial.sh
    [skip ci]

--- _scripts/oficial.sh
+++ _scripts/oficial.sh
@@ -291,13 +291,13 @@ itatiba "${itatiba_url}" "ITATIBA" "11504381"
 sleep 3
 boituva "${boituva_url}" "BOITUVA" "11504381"
 sleep 3
-jundiai "${jundiai_url}" "JUNDIAI" "11504381 449542698"
+jundiai "${jundiai_url}" "JUNDIAI" "11504381"
 sleep 3
 jandira "${jandira_url}" "JANDIRA" "11504381"
 sleep 3
-barueri "${barueri_url}" "BARUERI" "11504381 449542698"
+barueri "${barueri_url}" "BARUERI" "11504381"
 sleep 3
-aracoiaba "${aracoiaba_url}" "ARACOIABA" "11504381 449542698"
+aracoiaba "${aracoiaba_url}" "ARACOIABA" "11504381"
 sleep 3
 
 #adicionar mais cidades a cima
